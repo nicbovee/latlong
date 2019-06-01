13d951bb204d08910915cc502191b86efee9048f
Nicolas Bovee <admin@ghettifish.com>
2019-06-01 07:06:11 +0200

    Fixed lat and lng

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import Map from './Map';
+
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
@@ -15,7 +17,10 @@ const initial = {
   coord1: "",
   coord2: "",
   first: "",
-  second: ""
+  second: "",
+  mapObjects: [],
+  extraLat: "",
+  extraLng: "",
 };
 class App extends React.Component {
   constructor(props) {
@@ -28,6 +33,7 @@ class App extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    let finalVal = [];
     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.coord1.replace(/ /g,"+")},&key=AIzaSyDJnouqtMzKUzV-TfyR5TblsZcAcGYSjsE`)
     .then(x => x.json())
     .then(x => {
@@ -35,66 +41,131 @@ class App extends React.Component {
       this.setState({
         first: x.results[0].geometry.location,
       })
+      finalVal.push({name: this.state.coord1.capitalize(), ...x.results[0].geometry.location})
     })
+
     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.coord2.replace(/ /g,"+")},&key=AIzaSyDJnouqtMzKUzV-TfyR5TblsZcAcGYSjsE`)
     .then(x => x.json())
     .then(x => {
       console.log(x)
+      finalVal.push({name: this.state.coord2.capitalize(), ...x.results[0].geometry.location})
       this.setState({
         second: x.results[0].geometry.location,
+        mapObjects: finalVal,
+      })
+      this.setState({
+        extraLat: this.state.first.lat,
+        extraLng: this.state.first.lng,
       })
     })
   }
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <div class="fadeInLeft animated intro">
-            <h1>
-              LatLong
-            </h1>
-            <p>Compare two places to get their relative coordinates.</p>
-          </div>
-        <main>
-          <Wrapper onSubmit={this.handleSubmit} className="fadeInLeft animated">
-            {!this.state.first && !this.state.second &&
-            <>
-              <StyledInput 
-                type="text" 
-                value={this.state.coord1}
-                placeholder="Enter the name of the first place (i.e. 'paris')" 
-                onChange={ (event) => this.setState({coord1: event.target.value})}
-              />
-              <br/>
-              <StyledInput 
-                type="text" 
-                value={this.state.coord2}
-                placeholder="Enter the name of the second place (i.e. 'colorado')" 
-                onChange={ (event) => this.setState({coord2: event.target.value})}
-              />
-              <Submit type="submit" value="Submit" />
-            </>
-            }
-          </Wrapper>
-        </main>
-        {this.state.first && this.state.second &&
-        <>
-          <Results>
-          <h2>Results:</h2>
-            <Result>With the latitude from {this.state.coord2.capitalize()} and longitude from {this.state.coord1.capitalize()}: {this.state.first.lat}, {this.state.second.lng}</Result>
-            <Result>With the latitude from {this.state.coord1.capitalize()} and longitude from {this.state.coord2.capitalize()}: {this.state.second.lat}, {this.state.first.lng}</Result>
-          </Results>
-          <StartOver onClick={() => {this.setState(initial)}}>Start over</StartOver>
-        </>
+      <Main>
+        <LeftCol>
+          <div className="App">
+          <header className="App-header">
+            <div class="fadeInLeft animated intro">
+              <h1>
+                LatLong
+              </h1>
+              <p>Compare two places to get their relative coordinates.</p>
+            </div>
+          <main>
+            <Wrapper onSubmit={this.handleSubmit} className="fadeInLeft animated">
+              {!this.state.first && !this.state.second &&
+              <>
+                <StyledInput 
+                  type="text" 
+                  value={this.state.coord1}
+                  placeholder="Enter the name of the first place (i.e. 'paris')" 
+                  onChange={ (event) => this.setState({coord1: event.target.value})}
+                />
+                <br/>
+                <StyledInput 
+                  type="text" 
+                  value={this.state.coord2}
+                  placeholder="Enter the name of the second place (i.e. 'colorado')" 
+                  onChange={ (event) => this.setState({coord2: event.target.value})}
+                />
+                <Submit type="submit" value="Submit" />
+              </>
+              }
+            </Wrapper>
+          </main>
+          {this.state.first && this.state.second &&
+          <>
+            <Results>
+            <h2>Results:</h2>
+              <Result>
+                <strong>
+                With the latitude from {this.state.coord1.capitalize()} and longitude from {this.state.coord2.capitalize()}:
+                </strong> 
+                {this.state.first.lat}, {this.state.second.lng}</Result>
+              <Result>
+              <strong>
+                With the latitude from {this.state.coord2.capitalize()} and longitude from {this.state.coord1.capitalize()}:
+              </strong>
+              {this.state.second.lat}, {this.state.first.lng}</Result>
+            </Results>
+            <StartOver onClick={() => {this.setState(initial)}}>Start over</StartOver>
+          </>
+          }
+          <Footer>
+            made with ♥ in paris by <a href="https://twitter.com/nicbovee">@nicbovee</a>
+          </Footer>
+          </header>
+        </div>
+   
+        </LeftCol>
+   
+        {!this.state.first && !this.state.second &&
+          <RightCol>
+            <p>Enter a few names of some places.</p>
+            
+          </RightCol>
         }
-        </header>
-  
-      </div>
+        { this.state.first && this.state.second &&
+        <div>
+          <Map mapObjects={this.state.mapObjects}/>
+        </div>
+          }
+
+      </Main>
+      
     );
   }
 }
 
 export default App;
+
+const Main = styled.main`
+  display: flex;
+  max-width: 100vw;
+`
+const LeftCol = styled.div`
+
+`
+const RightCol = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: #353535;
+  flex: 1;
+  color: #797171;
+`
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  color: white;
+  padding: 5px;
+  a {
+    color: white;
+  }
+  font-size: 12px;
+`
 const Wrapper = styled.form`
 display: flex;
 flex-direction: column;
@@ -156,8 +227,13 @@ const Results = styled.div`
   padding: 15px 20px;
   background: #252525;
   line-height: .5;
+  overflow-x: scroll;
+  max-width: 400px;
 `
 
 const Result = styled.p`
 font-size: 16px;
+word-break: break-word;
+line-height: 1.3;
+text-align: left;
 `
---
+++ src/Map.js
@@ -0,0 +1,32 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from "react";
+
+
+export class MapContainer extends React.Component {
+    render() {
+      return (
+        <Map
+        google={this.props.google}
+        style={{alignSelf: "flex-start", maxWidth: '59%', height: '100%'}}
+
+        zoom={2}
+        onClick={this.onMapClicked}
+        >
+            
+        <Marker
+            name={this.props.mapObjects[0].name}
+            position={{lat: this.props.mapObjects[0].lat, lng: this.props.mapObjects[1].lng}} 
+        />
+            <Marker
+            name={this.props.mapObjects[0].name}
+            position={{lat: this.props.mapObjects[1].lat, lng: this.props.mapObjects[0].lng}} 
+        />
+
+      </Map>
+      );
+    }
+  }
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyDJnouqtMzKUzV-TfyR5TblsZcAcGYSjsE')
+})(MapContainer)
